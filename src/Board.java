cc4c756ceeca0098274ded30b32b0e36f3838b76
Ibrahem Jasim Alsalih <127301769+ibrahimjasim@users.noreply.github.com>
2025-10-08 13:24:04 +0200

    Added my check full and Check win to the Board.java

--- src/Board.java
+++ src/Board.java
@@ -3,6 +3,7 @@ public class Board {
     private final char[][] grid;
     private static  final char EMPTY = ' ';
 
+
     // Constructor: create a new board of given size
     public Board(int size) {
         this.size = size;
@@ -10,22 +11,48 @@ public class Board {
         reset();
     }
 
-    // Rest the game to start a new game(r: rest / c : chart
+    // Rest the game to start a new game
     public void reset() {
-        for (int r = 0; r < size; r++) {
-            for (int c = 0; c < size; c++) {
-                grid[r][c] = EMPTY;
+        for (int i = 0; i < size; i++) {
+            for (int j = 0; j < size; j++) {
+                grid[i][j] = EMPTY;
             }
         }
     }
-    // Place a plyaer's symbol (X or O)
+    // Check if a specific cell is empty
+    public boolean isCellEmpty(int row, int col) {
+        return grid[row][col] == EMPTY;
+    }
+
+    // Places a player's symbol (X or O) on the grid
     public void placeSymbol(int row, int col, char symbol) {
         grid[row][col] = symbol;
     }
-
-    // Check if a cell is empty
-    public boolean checkSymbol(int row, int col) {
-        return grid[row][col] == EMPTY;
+    // Check if the board is full
+    public boolean isFull() {
+        for (int i =0; i < size; i++) {
+            for (int j = 0; j < size; j++) {
+                if (grid[i][j] == EMPTY) return false;
+            }
+        }
+        return true;
     }
+
+    public boolean CheckWin(char symbol) {
+        // Check all rows
+        for (int i =0; i<size; i++){
+            boolean rowWin = true;
+            for (int j = 0; j<size; j++){
+                if (grid[i][j] != symbol) rowWin = false;
+            }
+            if (rowWin) return true;
+
+
+        }
+        return false;
+    }
+
+
+
 
 }

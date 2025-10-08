52d0a0314469777cce43d9e4562947213a48749c
Ibrahem Jasim Alsalih <127301769+ibrahimjasim@users.noreply.github.com>
2025-10-08 11:37:29 +0200

    Added my rest game and place symbol methods and a check a cell the Board.java

--- src/Board.java
+++ src/Board.java
@@ -1,2 +1,31 @@
 public class Board {
+    private final int size;
+    private final char[][] grid;
+    private static  final char EMPTY = ' ';
+
+    // Constructor: create a new board of given size
+    public Board(int size) {
+        this.size = size;
+        this.grid = new char[size][size];
+        reset();
+    }
+
+    // Rest the game to start a new game(r: rest / c : chart
+    public void reset() {
+        for (int r = 0; r < size; r++) {
+            for (int c = 0; c < size; c++) {
+                grid[r][c] = EMPTY;
+            }
+        }
+    }
+    // Place a plyaer's symbol (X or O)
+    public void placeSymbol(int row, int col, char symbol) {
+        grid[row][col] = symbol;
+    }
+
+    // Check if a cell is empty
+    public boolean checkSymbol(int row, int col) {
+        return grid[row][col] == EMPTY;
+    }
+
 }

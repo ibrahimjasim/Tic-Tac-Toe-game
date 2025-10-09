f3198a46b2bc86c89af31686f89a933c37bb357a
Ibrahem Jasim Alsalih <127301769+ibrahimjasim@users.noreply.github.com>
2025-10-09 12:17:29 +0200

    Create my move class in Move.java and set a Constructor to create a new move

--- src/Move.java
+++ src/Move.java
@@ -1,2 +1,13 @@
+//class to hold the row and column of a player's move
 public class Move {
+
+    public final int row;
+    public final int col;
+
+    // Constructor to create a new Move
+    public Move(int row, int col) {
+        this.row = row;
+        this.col = col;
+    }
+
 }

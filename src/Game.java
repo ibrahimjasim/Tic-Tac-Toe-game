7df8085680f84cde67f3bd6193275ed8ae9c068e
Ibrahem Jasim Alsalih <127301769+ibrahimjasim@users.noreply.github.com>
2025-10-09 14:18:02 +0200

    Added the basic for the game class and add it constructor

--- src/Game.java
+++ src/Game.java
@@ -1,2 +1,25 @@
 public class Game {
+    private final Board board;
+    private final Player player1;
+    private final Player player2;
+
+    private int p1wins = 0; // win counter for player 1
+    private int p2wins = 0;// win counter for player 2
+
+    // Constructor to set up a new game
+    public Game(Board board, Player player1, Player player2) {
+        this.board = board;
+        this.player1 = player1;
+        this.player2 = player2;
+    }
+
+    public void start() {
+        player current = player1;
+
+        while (true) {
+            board.printBoard();
+            System.out.println(current.getName() + "'s turn (" + current.getSymbol() + ")");
+        }
+
+    }
 }

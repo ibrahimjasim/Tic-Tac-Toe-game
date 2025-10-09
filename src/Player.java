4b198ef5135f6ef9d69f2a99786b5224511d7136
Ibrahem Jasim Alsalih <127301769+ibrahimjasim@users.noreply.github.com>
2025-10-09 12:27:39 +0200

    Create abstract class for player and set the constructor of it. Added the mothods to return player name as well as the symbol

--- src/Player.java
+++ src/Player.java
@@ -1,2 +1,26 @@
-public class Player {
+//Abstract base class for both human and computer players
+
+public abstract class Player {
+    protected final String name;   // The player's name
+    protected final char symbol;   // The player's symbol (X or O)
+
+    // Constructor: sets the name and symbol
+    public Player(String name, char symbol) {
+        this.name = name;
+        this.symbol = symbol;
+    }
+
+    // Returns the player's name
+    public String getName() {
+        return name;
+    }
+
+    // Returns the player's symbol
+    public char getSymbol() {
+        return symbol;
+    }
+
+    //  method: each type of player decides its own move
+    public abstract Move getMove(Board board);
 }
+

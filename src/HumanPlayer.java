d945a294fb77bff03ef44d451c07930ab85c2295
Ibrahem Jasim Alsalih <127301769+ibrahimjasim@users.noreply.github.com>
2025-10-09 12:37:53 +0200

    Added the HumenPlyer class and the constructor of it. create the method that ask the user to enter a valid row and col

--- src/HumanPlayer.java
+++ src/HumanPlayer.java
@@ -1,2 +1,22 @@
-public class HumanPlayer {
+import java.util.Scanner;
+
+public class HumanPlayer extends Player {
+
+    private final Scanner scanner;
+
+
+    public HumanPlayer(String name, char symbol, Scanner scanner) {
+        super(name, symbol);
+        this.scanner = scanner;
+    }
+
+    // Asks the user for their move and validates the input
+    @Override
+    public Move getMove(Board board) {
+        while (true) {
+            System.out.println(name +" (" + symbol + "), enter row and column ");
+            String input = scanner.nextLine();
+            String[] parts = input.trim().split(" ");
+        }
+    }
 }

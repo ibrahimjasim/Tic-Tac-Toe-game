65614e2218669ac31996813572939f7263351ccd
Ibrahem Jasim Alsalih <127301769+ibrahimjasim@users.noreply.github.com>
2025-10-09 12:50:27 +0200

    Create ComPlayer class extend from player. Added getMove to pick a random move and create an arrylist with a for loop to loop thorw the empty spots and pick a spot in the end.

--- src/ComPlayer.java
+++ src/ComPlayer.java
@@ -1,2 +1,35 @@
-public class ComPlayer {
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Random;
+
+public class ComPlayer extends Player {
+
+    // Computer player that picks random empty spots
+    private final Random random = new Random();
+
+    public ComPlayer(String name, char symbol) {
+        super(name, symbol);
+    }
+
+    // Chooses a random empty cell on the board
+    @Override
+    public Move getMove(Board board) {
+        List<Move> emptyCells = new ArrayList<>();
+
+        // Find all empty cells
+        for (int i = 0; i < board.getSize(); i++) {
+            for (int j = 0; j < board.getSize(); j++) {
+                if(board.isCellEmpty(i, j)){
+                    emptyCells.add(new Move(i, j));
+                }
+            }
+        }
+
+        // Pick one random move from the list
+        Move move = emptyCells.get(random.nextInt(emptyCells.size()));
+        System.out.println(name + "(" + symbol + ") choose: " + (move.row + 1) + " " + (move.col+1));
+        return move;
+    }
+
+
 }
--- src/HumanPlayer.java
+++ src/HumanPlayer.java
@@ -17,6 +17,27 @@ public class HumanPlayer extends Player {
             System.out.println(name +" (" + symbol + "), enter row and column ");
             String input = scanner.nextLine();
             String[] parts = input.trim().split(" ");
+
+            // Must have exactly two numbers
+            if (parts.length != 2) {
+                System.out.println("❌ Please enter two numbers separated by space.");
+                continue;
+            }
+
+            try {
+                // Convert strings into integers
+                int row = Integer.parseInt(parts[0]) -1;
+                int col = Integer.parseInt(parts[1]) -1;
+
+                // Check if inside the board range
+                if (row >=0 && col >= 0 && row < board.getSize() && col < board.getSize() ) {
+                    return new Move(row, col);
+                }else {
+                    System.out.println("❌ Out of range. Try again.");
+                }
+            }catch (NumberFormatException e) {
+                System.out.println("❌ Invalid numbers. Try again.");
+            }
         }
     }
 }
